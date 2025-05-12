c06352ce7c0502db020f3f6af108666cae970d1b
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-05-13 01:36:15 +0300

    Add some code for color and custom

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:open_fashion/core/colors.dart';
+
+import '../components/custom_appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,8 +12,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      backgroundColor: AppColors.primary,
+      appBar: CustomAppbar(
+          isBlackk:true
+      ),
 
+      body: Stack(
+        children: [
+          Positioned(
+              top: 10,
+              left: 0,
+              right: 0,
+              child: SvgPicture.asset("assets/texts/10.svg")),
+          Positioned(
+              top: 85,
+              left: 0,
+              right: 0,
+              child: SvgPicture.asset("assets/texts/Collection.svg")),
+          Positioned(
+              top: 40,
+              left: 0,
+              right: 0,
+              child: SvgPicture.asset("assets/texts/October.svg")),
+
+          Column(
+            children: [
+              Gap(120),
+              Image.asset("assets/cover/cover1.png"),
+              Gap(20),
+            ],
+          )
+        ],
       ),
     );
   }
---
+++ lib/components/custom_appbar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:open_fashion/core/colors.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppbar({super.key, required this.isBlackk});
+  final bool isBlackk;
+  @override
+  Widget build(BuildContext context) {
+   bool isBlack = isBlackk;
+    return Padding(
+      padding: const EdgeInsets.all(2),
+      child: AppBar(
+        backgroundColor: isBlack ? AppColors.primary : Colors.white,
+        centerTitle: true,
+        leadingWidth: 25,
+        leading: SvgPicture.asset("assets/svgs/Menu.svg",color: isBlack ? Colors.white: AppColors.primary),
+        title:  SvgPicture.asset("assets/logo/logo-bg.svg",color: isBlack ? Colors.white: AppColors.primary) ,
+
+        actions: [
+          SvgPicture.asset("assets/svgs/Search.svg",color: isBlack ? Colors.white: AppColors.primary),
+          Gap(20),
+          SvgPicture.asset("assets/svgs/shopping bag.svg",color: isBlack ? Colors.white: AppColors.primary)
+        ],
+      ),
+    );
+  }
+
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(80);
+}
